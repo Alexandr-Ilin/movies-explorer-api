d7e598f096b35220906b3c46087c5932224d5bdf
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-09-01 17:36:33 +0300

    route user

---
+++ controllers/movie.js
@@ -0,0 +1,20 @@
+const movie = require('../models/movie');
+
+const createMovie = (req, res, next) => {
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    movieId,
+    nameRU,
+    nameEN,
+  } = req.body;
+
+};
+
+module.exports = { createMovie };
